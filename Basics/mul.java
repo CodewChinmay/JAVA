18f51b4d099cb6f8d8668ed972743bb217dbe32d
ingalechinnmay <ingalechinmay12@gmail.com>
2024-08-10 09:34:25 +0530

    Multiplication program of two number

---
+++ Basics/mul.java
@@ -0,0 +1,8 @@
+class Mul {
+    public static void main(String args[]) {
+        int a = 6;
+        int b = 2;
+        int c = a * b;
+        System.out.println("Multiplication of two numbers: " + c);
+    }
+}
