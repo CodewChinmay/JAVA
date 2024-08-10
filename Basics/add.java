275b1676253a5ebb1d3dc1f4471cac96e3b4420e
ingalechinnmay <ingalechinmay12@gmail.com>
2024-08-10 09:20:10 +0530

    Addition of Two Programs

---
+++ Basics/add.java
@@ -0,0 +1,9 @@
+class Add {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 2;
+        int c = a + b;
+        System.out.println("Sum of Two number is : " + c);
+    }
+    
+}
